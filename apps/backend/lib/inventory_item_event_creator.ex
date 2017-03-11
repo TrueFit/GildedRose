8564916f6430d0ba5b9edd24d1e05b2d24738891
b1collector <b1collector@gmail.com>
2017-03-11 14:54:38 -0500

    Move unit tests to DocTests
    To making testing easier, I am going to try to organize my tests such that
    unit tests go in doc tests and integration tests go in test files.

--- apps/backend/lib/inventory_item_event_creator.ex
+++ apps/backend/lib/inventory_item_event_creator.ex
@@ -4,9 +4,34 @@ defmodule InventoryItemEventCreator do
   alias Event.ItemAddedToInventory, as: Added
 
   @doc """
-  Validate command and create an ItemAddedToInventory event.
+  Create an ItemAddedToInventory event if provided with valid input.
+
+  ## Example
+      iex> InventoryItemEventCreator.item_added_to_inventory("", :food, 10, 10)
+      {:error, :invalid_name}
+
+      iex> InventoryItemEventCreator.item_added_to_inventory("banana", :bad, 10, 10)
+      {:error, :invalid_category}
+
+      iex> InventoryItemEventCreator.item_added_to_inventory("banana", :food, "abc", 10)
+      {:error, :invalid_sell_in}
+
+      iex> InventoryItemEventCreator.item_added_to_inventory("banana", :food, 10, -10)
+      {:error, :invalid_quality}
+
+      iex> InventoryItemEventCreator.item_added_to_inventory("banana", :food, 10, 80)
+      {:error, :invalid_quality}
+
+      iex> InventoryItemEventCreator.item_added_to_inventory("awesome", :sulfuras, 10, 150)
+      {:error, :invalid_quality}
+
+      iex> InventoryItemEventCreator.item_added_to_inventory("banana", :food, 10, 10)
+      {:ok, %Event.ItemAddedToInventory{category: :food, name: "banana", quality: 10, sell_in: 10}}
+
+      iex> InventoryItemEventCreator.item_added_to_inventory("awesome", :sulfuras, 10, 80)
+      {:ok, %Event.ItemAddedToInventory{category: :sulfuras, name: "awesome", quality: 80, sell_in: 10}}
   """
-  @spec item_added_to_inventory(String.t, atom, integer, integer) :: {:ok, Added.t} | {:error, String.t}
+  @spec item_added_to_inventory(String.t, atom, integer, integer) :: {:ok, Added.t} | {:error, atom}
   def item_added_to_inventory(name, category, sell_in, quality) do
     with {:ok, n} <- validate_name(name),
          {:ok, c} <- validate_category(category),
--- apps/backend/test/inventory_item_event_creator_test.exs
+++ apps/backend/test/inventory_item_event_creator_test.exs
@@ -2,45 +2,4 @@ defmodule InventoryItemEventCreatorTest do
   use ExUnit.Case
   doctest InventoryItemEventCreator
 
-  test "add item to inventory will error for invalid name" do
-    {:error, :invalid_name} = InventoryItemEventCreator.item_added_to_inventory("", :food, 10, 10)
-  end
-
-  test "add item to inventory will error for invalid category" do
-    {:error, :invalid_category} = InventoryItemEventCreator.item_added_to_inventory("banana", :bad, 10, 10)
-  end
-
-  test "add item to inventory will error for string sell_in" do
-    {:error, :invalid_sell_in} = InventoryItemEventCreator.item_added_to_inventory("banana", :food, "abc", 10)
-  end
-
-  test "add item to inventory will error for negative quality" do
-    {:error, :invalid_quality} = InventoryItemEventCreator.item_added_to_inventory("banana", :food, 10, -10)
-  end
-
-  test "add item to inventory will error for excessive quality" do
-    {:error, :invalid_quality} = InventoryItemEventCreator.item_added_to_inventory("banana", :food, 10, 80)
-  end
-
-  test "Given an item with category sulfuras and quality 40 when the item is attempted to be added to inventory then a malformed error occurs" do
-    {:error, :invalid_quality} = InventoryItemEventCreator.item_added_to_inventory("awesome", :sulfuras, 10, 150)
-  end
-
-  test "add item to inventory will create item added to inventory event" do
-    {:ok, event} = InventoryItemEventCreator.item_added_to_inventory("banana", :food, 10, 10)
-
-    assert(event.name == "banana")
-    assert(event.category == :food)
-    assert(event.sell_in == 10)
-    assert(event.quality == 10)
-  end
-
-  test "Given a sulfuras item with quality 80 when the item is attempted to be added to inventory then an item added to inventory event is raised" do
-    {:ok, event} = InventoryItemEventCreator.item_added_to_inventory("awesome", :sulfuras, 10, 80)
-
-    assert(event.name == "awesome")
-    assert(event.category == :sulfuras)
-    assert(event.sell_in == 10)
-    assert(event.quality == 80)
-  end
 end
