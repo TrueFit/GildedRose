e265ee50179c5f483c5f179c115526168f6333d2
b1collector <b1collector@gmail.com>
2017-03-12 15:27:47 -0400

    Connect CommandStoreState to EventStore

--- apps/backend/lib/command_projection.ex
+++ apps/backend/lib/command_projection.ex
@@ -2,11 +2,23 @@ defmodule CommandStateStore do
   @moduledoc false
   use GenServer
 
+  @subscription "command_subscription"
+
   # CLIENT
 
   @spec start_link() :: {:ok, any}
   def start_link do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+    with {:ok, pid} <- GenServer.start_link(__MODULE__, %{}, name: __MODULE__),
+         {:ok, _subscription} <- EventStore.subscribe_to_all_streams(@subscription, pid),
+    do: {:ok, pid}
+  end
+
+  def stream_exists?(item_id) do
+    GenServer.call(__MODULE__, {:stream_exists, item_id})
+  end
+
+  def inventory_item_state(item_id) do
+    GenServer.call(__MODULE__, {:get_item, item_id})
   end
 
   @spec next_id() :: String.t
@@ -19,5 +31,21 @@ defmodule CommandStateStore do
   @spec init(any) :: {:ok, map}
   def init(_) do
     {:ok, %{}}
+  end
+
+  def handle_call({:stream_exists, stream_id}, _from, state) do
+    {:reply, Map.has_key?(state, stream_id), state}
+  end
+
+  def handle_call({:get_item, item_id}, _from, state) do
+    {:reply, Map.get(state, item_id), state}
+  end
+
+  def handle_info({:events, events, subscription}, state) do
+    send(subscription, {:ack, List.last(events).event_id})
+
+    IO.inspect(List.first(events))
+
+    {:noreply, state}
   end
 end
