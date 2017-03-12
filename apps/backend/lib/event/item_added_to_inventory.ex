e42ba8eb47eb7b2f36fb374d312d70c181130fb0
b1collector <b1collector@gmail.com>
2017-03-12 17:22:26 -0400

    Represent category with string
    It serializes better

--- apps/backend/lib/event/item_added_to_inventory.ex
+++ apps/backend/lib/event/item_added_to_inventory.ex
@@ -2,7 +2,7 @@ defmodule Event.ItemAddedToInventory do
   @moduledoc """
   Data structure to record an item being added to inventory.
   """
-  @type t :: %Event.ItemAddedToInventory{name: String.t, category: atom, sell_in: integer, quality: integer}
+  @type t :: %Event.ItemAddedToInventory{name: String.t, category: String.t, sell_in: integer, quality: integer}
 
-  defstruct name: "", category: :unknown, sell_in: 0, quality: 0
+  defstruct name: "", category: "", sell_in: 0, quality: 0
 end
