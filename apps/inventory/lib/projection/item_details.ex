2965c919c607b1fb567ef4f58646d448b108f2af
b1collector <b1collector@gmail.com>
2017-03-19 14:42:39 -0400

    Project DayPassed event
    ItemDetails projection includes DayPassed event.

--- apps/inventory/lib/projection/item_details.ex
+++ apps/inventory/lib/projection/item_details.ex
@@ -2,24 +2,29 @@ defmodule Inventory.Projection.ItemDetails do
   @moduledoc false
   @type t :: %Inventory.Projection.ItemDetails{}
 
+  alias Inventory.Projection.ItemDetails
+  alias Inventory.Domain.Item
+  alias Inventory.Event
+
   defstruct item_id: "", version: 0, name: "", category: "", sell_in: 0, quality: 0
 
   defimpl Inventory.Projection do
-    @spec project(Inventory.Projection.ItemDetails.t, %EventStore.EventData{}) :: Inventory.Projection.ItemDetails.t
+    @spec project(ItemDetails.t, %EventStore.EventData{}) :: ItemDetails.t
     def project(p, %EventStore.RecordedEvent{data: event, metadata: %{"item_id" => id}, stream_version: v}) do
-      %Inventory.Projection.ItemDetails{p | item_id: id, version: v}
+      %ItemDetails{p | item_id: id, version: v}
       |> set_event(event)
     end
 
-    @spec set_event(Inventory.Projection.ItemDetails.t, struct) :: Inventory.Projection.ItemDetails.t
-    defp set_event(p, %Inventory.Event.ItemAdded{name: n, category: c, sell_in: s, quality: q}) do
-      %Inventory.Projection.ItemDetails{p | name: n, category: c, sell_in: s, quality: q}
+    @spec set_event(ItemDetails.t, struct) :: ItemDetails.t
+    defp set_event(p, %Event.ItemAdded{name: n, category: c, sell_in: s, quality: q}) do
+      %ItemDetails{p | name: n, category: c, sell_in: s, quality: q}
     end
-    defp set_event(p, %Inventory.Event.ItemNameChanged{name: n}), do: %Inventory.Projection.ItemDetails{p | name: n}
-    defp set_event(p, %Inventory.Event.DayPassed{}), do: process_day(p)
+    defp set_event(%ItemDetails{name: n, category: c, sell_in: s, quality: q} = p, %Event.DayPassed{}) do
+      {_, _, s!, q!} = Item.age({n, c, s, q})
+
+      %ItemDetails{p | sell_in: s!, quality: q!}
+    end
+    defp set_event(p, %Event.ItemNameChanged{name: n}), do: %Inventory.Projection.ItemDetails{p | name: n}
     defp set_event(p, _), do: p
-
-
-    defp process_day(%Inventory.Projection.ItemDetails{category: "sulfuras"} = p), do: p
   end
 end
