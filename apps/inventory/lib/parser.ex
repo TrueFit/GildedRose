0e1a481d64140cf6dae00c5e23f7deeb1a48de7b
b1collector <b1collector@gmail.com>
2017-06-10 20:08:34 -0400

    Parse file stream

--- apps/inventory/lib/parser.ex
+++ apps/inventory/lib/parser.ex
@@ -1,7 +1,7 @@
 defmodule Inventory.Parser do
   @moduledoc false
 
-  alias Inventory.Parser.ParsingError
+  alias Inventory.Parser.Error
   alias Inventory.Parser.Item
 
   @doc """
@@ -13,14 +13,22 @@ defmodule Inventory.Parser do
       %Inventory.Parser.Item{category: "weapon", name: "sword", quality: "26", sell_in: "12"}
 
       iex> Inventory.Parser.parse_line("bad line")
-      %Inventory.Parser.ParsingError{error: "Invalid string", line: "bad line"}
+      %Inventory.Parser.Error{error: "Invalid string", line: "bad line"}
   """
   def parse_line(line) do
     with [name, category, sell_in, quality] <- String.split(line, ",") do
       Item.create(String.trim(name), String.trim(category), String.trim(sell_in), String.trim(quality))
     else
-      _ -> ParsingError.create("Invalid string", line)
+      _ -> Error.create("Invalid string", line)
     end
+  end
+
+  @doc """
+  Transform a stream of strings into Parser structs (Item or Error)
+  """
+  def parse(stream) do
+    stream
+    |> Stream.map(&parse_line/1)
   end
 
 end
@@ -31,7 +39,7 @@ defmodule Inventory.Parser.Item do
   def create(n, c, s, q), do: %__MODULE__{name: n, category: c, sell_in: s, quality: q}
 end
 
-defmodule Inventory.Parser.ParsingError do
+defmodule Inventory.Parser.Error do
   defstruct error: "Unable to parse", line: ""
 
   def create(e, l), do: %__MODULE__{error: e, line: l}
