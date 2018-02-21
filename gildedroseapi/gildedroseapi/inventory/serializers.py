3223240d07b2263e8cc5bb7f2cd1383cee5f8411
Stephen Jacobs <stephenjacobs@gmail.com>
2018-02-21 15:45:11 -0500

    show item category name in table

--- gildedroseapi/gildedroseapi/inventory/serializers.py
+++ gildedroseapi/gildedroseapi/inventory/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
     class Meta:
         model = Item
-        fields = ('id', 'name', 'category', 'sell_in', 'quality')
+        fields = ('id', 'name', 'category', 'category_name', 'sell_in', 'quality')
