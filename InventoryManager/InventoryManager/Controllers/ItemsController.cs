d5f1200889fd37cc17d32af21ae3bf143b720353
alt0077 <alt0077@gmail.com>
2018-10-01 15:01:26 -0400

    Initial Checkin

---
+++ InventoryManager/InventoryManager/Controllers/ItemsController.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using PagedList;
+
+namespace InventoryManager.Controllers
+{
+    public class ItemsController : BaseController
+    {
+        // GET: Items
+        public ActionResult Index(Guid guid, string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            // get active items for this store only
+            var tbl_Items = db.tbl_Items.Where(x => x.StoreGuid == guid && x.Active == true && x.Quality > 0 && (x.DateTrashed >= DateTime.Now || x.DateTrashed == null)).Include(t => t.tbl_Stores);
+
+            // set store in session
+            currentStore = db.tbl_Stores.Where(x => x.Guid == guid).FirstOrDefault();
+            System.Web.HttpContext.Current.Session["currentStore"] = currentStore;
+            ViewBag.Store = currentStore;
+
+            // Search Name
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                tbl_Items = tbl_Items.Where(s => s.Name.Contains(searchString) || s.Category.Contains(searchString));
+            }
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            // sorting
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = sortOrder == "Name" ? "Name_desc" : "Name";
+            ViewBag.CategorySortParm = sortOrder == "Category" ? "Category_desc" : "Category";
+            ViewBag.SellInSortParm = sortOrder == "SellIn" ? "SellIn_desc" : "SellIn";
+            ViewBag.QualitySortParm = sortOrder == "Quality" ? "Quality_desc" : "Quality";
+            ViewBag.LegendarySortParm = sortOrder == "Legendary" ? "SellIn_desc" : "Legendary";
+            ViewBag.BetterWithAgeSortParm = sortOrder == "BetterWithAge" ? "BetterWithAge_desc" : "BetterWithAge";
+            switch (sortOrder)
+            {
+                case "Name":
+                    tbl_Items = tbl_Items.OrderBy(s => s.Name);
+                    break;
+                case "Name_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.Name);
+                    break;
+                case "Category":
+                    tbl_Items = tbl_Items.OrderBy(s => s.Category);
+                    break;
+                case "Category_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.Category);
+                    break;
+                case "SellIn":
+                    tbl_Items = tbl_Items.OrderBy(s => s.SellIn);
+                    break;
+                case "SellIn_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.SellIn);
+                    break;
+                case "Quality":
+                    tbl_Items = tbl_Items.OrderBy(s => s.Quality);
+                    break;
+                case "Quality_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.Quality);
+                    break;
+                case "Legendary":
+                    tbl_Items = tbl_Items.OrderBy(s => s.Legendary);
+                    break;
+                case "Legendary_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.Legendary);
+                    break;
+                case "BetterWithAge":
+                    tbl_Items = tbl_Items.OrderBy(s => s.BetterWithAge);
+                    break;
+                case "BetterWithAge_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.BetterWithAge);
+                    break;
+                default:
+                    tbl_Items = tbl_Items.OrderBy(s => s.Name);
+                    break;
+            }
+
+            int pageSize = 20;
+            int pageNumber = (page ?? 1);
+            return View(tbl_Items.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Trash Items
+        public ActionResult Trash(Guid guid, string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            // get quality = 0 items for this store only
+            var tbl_Items = db.tbl_Items.Where(x => x.StoreGuid == guid && x.Active == true && x.Quality == 0).Include(t => t.tbl_Stores);
+
+            // set store in session
+            currentStore = db.tbl_Stores.Where(x => x.Guid == guid).FirstOrDefault();
+            System.Web.HttpContext.Current.Session["currentStore"] = currentStore;
+            ViewBag.Store = currentStore;
+
+            // Search Name
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                tbl_Items = tbl_Items.Where(s => s.Name.Contains(searchString) || s.Category.Contains(searchString));
+            }
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            // sorting
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = sortOrder == "Name" ? "Name_desc" : "Name";
+            ViewBag.CategorySortParm = sortOrder == "Category" ? "Category_desc" : "Category";
+            ViewBag.SellInSortParm = sortOrder == "SellIn" ? "SellIn_desc" : "SellIn";
+            ViewBag.QualitySortParm = sortOrder == "Quality" ? "Quality_desc" : "Quality";
+            ViewBag.LegendarySortParm = sortOrder == "Legendary" ? "SellIn_desc" : "Legendary";
+            ViewBag.BetterWithAgeSortParm = sortOrder == "BetterWithAge" ? "BetterWithAge_desc" : "BetterWithAge";
+            switch (sortOrder)
+            {
+                case "Name":
+                    tbl_Items = tbl_Items.OrderBy(s => s.Name);
+                    break;
+                case "Name_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.Name);
+                    break;
+                case "Category":
+                    tbl_Items = tbl_Items.OrderBy(s => s.Category);
+                    break;
+                case "Category_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.Category);
+                    break;
+                case "SellIn":
+                    tbl_Items = tbl_Items.OrderBy(s => s.SellIn);
+                    break;
+                case "SellIn_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.SellIn);
+                    break;
+                case "Quality":
+                    tbl_Items = tbl_Items.OrderBy(s => s.Quality);
+                    break;
+                case "Quality_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.Quality);
+                    break;
+                case "Legendary":
+                    tbl_Items = tbl_Items.OrderBy(s => s.Legendary);
+                    break;
+                case "Legendary_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.Legendary);
+                    break;
+                case "BetterWithAge":
+                    tbl_Items = tbl_Items.OrderBy(s => s.BetterWithAge);
+                    break;
+                case "BetterWithAge_desc":
+                    tbl_Items = tbl_Items.OrderByDescending(s => s.BetterWithAge);
+                    break;
+                default:
+                    tbl_Items = tbl_Items.OrderBy(s => s.Name);
+                    break;
+            }
+
+            int pageSize = 20;
+            int pageNumber = (page ?? 1);
+            return View(tbl_Items.ToPagedList(pageNumber, pageSize));
+        }
+
+        // clear all trash
+        public ActionResult TrashAll(Guid guid)
+        {
+            // get trash items for this store only
+            var tbl_Items = db.tbl_Items.Where(x => x.StoreGuid == guid && x.Active == true && x.Quality == 0).Include(t => t.tbl_Stores);
+            
+            foreach (tbl_Items item in tbl_Items.ToList())
+            {
+                // deactive trash items
+                item.Active = false;
+                item.DateTrashed = DateTime.Now;
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            tbl_Items = db.tbl_Items.Where(x => x.StoreGuid == guid && x.Active == true && x.Quality == 0).Include(t => t.tbl_Stores);
+            return RedirectToAction("Trash", new { guid = currentStore.Guid });
+        }
+
+        // clear all trash
+        public ActionResult Sell(Guid guid)
+        {
+            // get trash items for this store only
+            var tbl_Items = db.tbl_Items.Where(x => x.Guid == guid);
+
+            foreach (tbl_Items item in tbl_Items.ToList())
+            {
+                // deactive sold items
+                item.Active = false;
+                item.DateSold = DateTime.Now;
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", new { guid = currentStore.Guid });
+        }
+
+        // to run the Daily Update Job manually
+        public ActionResult DailyUpdate()
+        {
+            JobSchedulerController JS = new JobSchedulerController();
+            if (JS.DailyUpdateAlgorithm(true))
+            {
+                // ran successfully
+                return RedirectToAction("Admin", "Home", new { message = "Daily Update Successfully Run!" });
+            }
+            else
+            {
+                // failed to complete
+                tbl_DailyUpdateLog log = db.tbl_DailyUpdateLog.OrderByDescending(x => x.DateTime).FirstOrDefault();
+                string msg = "Daily Update Failed! Only runs once per day! Last Run: " + log.DateTime;
+                return RedirectToAction("Admin", "Home", new { message = msg });
+            } 
+        }
+
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Guid,StoreGuid,Name,Category,SellIn,Quality,Legendary,BetterWithAge,Image,ImageName,DateCreated,DateTrashed,Active")] tbl_Items tbl_Items)
+        {
+            if (ModelState.IsValid)
+            {
+                tbl_Items.Guid = Guid.NewGuid();
+                tbl_Items.StoreGuid = currentStore.Guid;
+                tbl_Items.DateCreated = DateTime.Now;
+                if (tbl_Items.Name == "Aged Brie")
+                    { tbl_Items.BetterWithAge = true; }
+                if (tbl_Items.Category == "Sulfuras")
+                    { tbl_Items.Legendary = true; }
+                tbl_Items.Active = true;
+                db.tbl_Items.Add(tbl_Items);
+                db.SaveChanges();
+                return RedirectToAction("Index", new { guid = currentStore.Guid });
+            }
+
+            return View(tbl_Items);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Items tbl_Items = db.tbl_Items.Find(id);
+            if (tbl_Items == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Items);
+        }
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Guid,StoreGuid,Name,Category,SellIn,Quality,PurchasePrice,SellPrice,Legendary,BetterWithAge,Image,ImageName,DateCreated,DateTrashed,Active")] tbl_Items tbl_Items)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_Items).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", new { guid = currentStore.Guid });
+            }
+            return View(tbl_Items);
+        }
+
+        // GET: Upload
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        // POST: Upload
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(string file)
+        {
+            try
+            {
+                using (StringReader reader = new StringReader(file))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] itemimport = line.Split(',');
+
+                        tbl_Items item = new tbl_Items();
+                        item.Guid = Guid.NewGuid();
+                        item.Active = true;
+                        item.Category = itemimport[1];
+                        item.DateCreated = DateTime.Now;
+                        item.Name = itemimport[0];
+                        if(item.Name == "Aged Brie")
+                            { item.BetterWithAge = true; }
+                        if (item.Category == "Sulfuras")
+                            { item.Legendary = true; }
+                        item.Quality = Int32.Parse(itemimport[2]);
+                        item.SellIn = Int32.Parse(itemimport[3]);
+                        item.StoreGuid = currentStore.Guid;
+                        db.tbl_Items.Add(item);
+                        db.SaveChanges();
+                    }
+                }
+           
+                ViewBag.Message = "Inventory Uploaded Successfully!";
+                return View();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "Inventory upload failed! " + e.Message;
+                return View();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ InventoryManager/InventoryManager/Controllers/JobSchedulerController.cs
@@ -0,0 +1,171 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+
+namespace InventoryManager.Controllers
+{
+    public class JobSchedulerController : Controller
+    {
+        public InventoryManagerEntities db = new InventoryManagerEntities();
+
+        public async Task<ActionResult> Start()
+        {
+            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            IScheduler scheduler = await schedulerFactory.GetScheduler();
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<UpdateJob>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("UpdateJob ", "GreetingGroup")
+                // run every 13th hour of every day
+                .WithDailyTimeIntervalSchedule(s => s
+                  .OnEveryDay()
+                  .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(13, 00))
+                  .EndingDailyAfterCount(1))
+                .StartAt(DateTime.UtcNow)
+                .WithPriority(1)
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+            return View();
+        }
+
+        /// <summary>
+        /// SimpleJOb is just a class that implements IJOB interface. It implements just one method, Execute method
+        /// </summary>
+        public class UpdateJob : IJob
+        {
+            public async Task Execute(IJobExecutionContext context)
+            {
+                JobSchedulerController JS = new JobSchedulerController();
+                bool run = JS.DailyUpdateAlgorithm(false);
+            }
+        }
+
+        // Schedule Job
+        public void ScheduleJob()
+        {
+            // construct a scheduler factory
+            ISchedulerFactory sf = new StdSchedulerFactory();
+
+            // get a scheduler, start the schedular before triggers or anything else
+            IScheduler sched = (IScheduler)sf.GetScheduler();
+            sched.Start();
+
+            // create job
+            IJobDetail job = JobBuilder.Create<UpdateJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+            // create trigger
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(5).RepeatForever())
+                .Build();
+
+            // Schedule the job using the job and trigger 
+            sched.ScheduleJob(job, trigger);
+        }
+
+        // logic to update quality on all items in all stores
+        public bool DailyUpdateAlgorithm(bool manualRun)
+        {
+            // get the date of the last run
+            tbl_DailyUpdateLog log = db.tbl_DailyUpdateLog.OrderByDescending(x => x.DateTime).FirstOrDefault();
+
+            // check to only run once per day
+            if (log == null || log.DateTime <= DateTime.Now.AddDays(-1))
+            {
+                // get trash items for this store only
+                var tbl_Items = db.tbl_Items.Where(x => x.Active == true && x.Quality > 0);
+
+                foreach (tbl_Items item in tbl_Items.ToList())
+                {
+                    if (item.Category == "Conjured" || DateTime.Now.AddDays(item.SellIn) < DateTime.Now)
+                        // SellIn < Today OR Conjured then Quality – 2
+                        item.Quality = item.Quality - 2;
+                    else if (item.BetterWithAge == true)
+                        // BetterWithAge “Aged Brie” THEN Quality +1
+                        item.Quality = item.Quality + 1;
+                    else if (item.Category == "Backstage Passes")
+                    {
+                        // BackstagePasses WHEN SellIn <= 10 THEN Quality +2, WHEN SellIn <= 5 THEN Quality +3, WHEN SellIn = 0 THEN Quality = 0
+                        if (item.SellIn <= 10)
+                            item.Quality = item.Quality + 2;
+                        else if (item.SellIn <= 5)
+                            item.Quality = item.Quality + 3;
+                        else if (item.SellIn == 0)
+                            item.Quality = 0;
+                    }
+                    else
+                    {
+                        if (item.Legendary == false)
+                        {
+                            // otherwise lower Quality - 1
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+
+                    // SellIn - 1 for all except Legendary
+                    if (item.Legendary == false)
+                    {
+                        item.SellIn = item.SellIn - 1;
+                    }
+
+                    // Quality is never greater than 50 unless Legendary
+                    if ((item.Legendary == false) && item.Quality > 50)
+                    {
+                        item.Quality = 50;
+                    }
+
+                    db.Entry(item).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                }
+
+                // log Daily run
+                tbl_DailyUpdateLog slogEntry = new tbl_DailyUpdateLog();
+                slogEntry.DateTime = DateTime.Now;
+                slogEntry.Guid = Guid.NewGuid();
+                if (manualRun)
+                {
+                    slogEntry.Type = "Success Manual";
+                }
+                else
+                {
+                    slogEntry.Type = "Success Automated";
+                }
+                db.tbl_DailyUpdateLog.Add(slogEntry);
+                db.SaveChanges();
+
+                // success
+                return true;
+                }
+
+                // log Daily run
+                tbl_DailyUpdateLog flogEntry = new tbl_DailyUpdateLog();
+                flogEntry.DateTime = DateTime.Now;
+                flogEntry.Guid = Guid.NewGuid();
+                if (manualRun)
+                {
+                    flogEntry.Type = "Failed Manual";
+                }
+                else
+                {
+                    flogEntry.Type = "Failed Automated";
+                }
+                db.tbl_DailyUpdateLog.Add(flogEntry);
+                db.SaveChanges();
+
+                // failed
+                return false;
+            }
+
+    }
+}
