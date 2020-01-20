c1deb3382bf22d8120727ac792809cab8fa54135
Matthew Haller <mphaller@gmail.com>
2020-01-20 16:41:45 -0500

    Fixup
    Remove unused comments and files

--- grose-ui/src/reducer.js
+++ grose-ui/src/reducer.js
@@ -30,16 +30,3 @@ export const appReducer = (state = initialState, action) => {
             return state
     }
 }
-
-// Create a Redux store holding the state of your app.
-// Its API is { subscribe, dispatch, getState }.
-// let store = createStore(appReducer);
-
-// The only way to mutate the internal state is to dispatch an action.
-// The actions can be serialized, logged or stored and later replayed.
-// store.dispatch({ type: 'INCREMENT' })
-// // 1
-// store.dispatch({ type: 'INCREMENT' })
-// // 2
-// store.dispatch({ type: 'DECREMENT' })
-// // 1
