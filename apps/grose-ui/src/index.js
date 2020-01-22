54ca63f888fbf1eeca47ec32b80155467acc3ee9
Matthew Haller <mphaller@gmail.com>
2020-01-21 22:39:03 -0500

    Remove ServiceWorker
    This is an appendix of create-react-app that won't be needed.

--- apps/grose-ui/src/index.js
+++ apps/grose-ui/src/index.js
@@ -6,11 +6,9 @@ import thunk from 'redux-thunk';
 
 import ConnectedApp from './components/ConnectedApp';
 import { appReducer, initialState } from './reducer';
-import * as serviceWorker from './serviceWorker';
 
 import './styles/index.css';
 
 const store = createStore(appReducer, initialState, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><ConnectedApp /></Provider>, document.getElementById('root'));
-serviceWorker.unregister();
