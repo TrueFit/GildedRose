cfd7912f3d688947e44aff36f5475453b1fbe070
Chris Rickard <crickard62@gmail.com>
2021-10-26 13:46:35 -0400

    Changes to support new layout and functionality

--- Services/InventoryService.cs
+++ Services/InventoryService.cs
@@ -13,7 +13,7 @@ namespace GildedRose.Services
     {
         private readonly IDataService _service;
         private readonly IItemFactory _factory;
-        private IEnumerable<IItem> _list;
+        private static IEnumerable<IItem> _list;
 
         public InventoryService(IDataService service, IItemFactory factory)
         {
@@ -21,9 +21,31 @@ namespace GildedRose.Services
             _factory = factory;
         }
 
+        public IEnumerable<IItem> ViewActiveInventory() 
+        {
+            // active items have non-zero Quality
+            return _list.Where(i => i.Quality != 0); 
+        }
+
+        public IEnumerable<IItem> ViewExpiredInventory()
+        {
+            // expired items have zero Quality
+            return _list.Where(i => i.Quality == 0);
+        }
+        public bool IsLoaded()
+        {
+            if ((_list != null))
+                // true if there are values; false otherwise. Which means we need to load...
+                return _list.Any();
+            else
+                return false;
+        }
+
         public async Task<IEnumerable<IItem>> GetInventory()
         {
             var reader = await _service.DownloadData();
+
+            //need a using here because we want to make sure we dispose of the reader
             using (reader)
             {
                 _list = await CreateItemList(reader);
@@ -34,6 +56,7 @@ namespace GildedRose.Services
 
         public IEnumerable<IItem> CloseOutDay()
         {
+            // call UpdateItem for each Item instance in the list
             foreach (var item in _list)
             {
                 item.UpdateItem();
@@ -41,10 +64,21 @@ namespace GildedRose.Services
 
             return _list;
         }
+
+        public IEnumerable<IItem> EmptyTrash()
+        {
+            //replace the existing list with a list that does not contain items with Quality 0
+            _list = _list.Where(i => i.Quality != 0).ToList();
+            return ViewExpiredInventory();
+        }
+
         private async Task<IEnumerable<IItem>> CreateItemList(StreamReader reader)
         {
             var records = new List<IItem>();
+
+            //need a using here because we want to make sure we dispose of the reader
             using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
             // there's no header in the test data file so we'll build a list by hand
             while (await csv.ReadAsync())
             {
