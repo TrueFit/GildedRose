ca93fdd897600c35bb4c6e91323cc43c4317e2b2
brianash <bash@mechanican.com>
2021-10-30 03:25:15 -0500

    Added Simple Crud ops to Inventory Controller Started Day Increment Handler

--- GildedRose/Controllers/InventoryController.cs
+++ GildedRose/Controllers/InventoryController.cs
@@ -11,7 +11,6 @@ namespace GildedRose.Controllers
     [Route("[controller]")]
     public class InventoryController : ControllerBase
     {
-
         private readonly InventoryContext _context;
 
         public InventoryController(InventoryContext context)
@@ -19,6 +18,7 @@ namespace GildedRose.Controllers
             _context = context;
         }
 
+        //Get All Items
         [HttpGet("{GetAllItems}")]
         public async Task<ActionResult<List<Item>>> GetAllItems()
         {
@@ -27,12 +27,45 @@ namespace GildedRose.Controllers
             return items;
         }
 
+        //Get a single item
         [HttpGet("{GetAnItem}/{itemId}")]
         public async Task<ActionResult<Item>> GetAnItem(int itemId)
         {
             var item = await _context.Items.AsNoTracking().FirstOrDefaultAsync(x => x.ItemId == itemId);
             if (item == null) return NotFound();
             return item;
+        }
+
+        //Add a single item
+        [HttpPost("{AddAnItem}")]
+        public async Task<ActionResult<Item>> AddAnItem([FromBody] Item item)
+        {
+            if (item.CategoryId <= 0) return BadRequest($"error: Category ID {item.CategoryId} is not valid");
+            if (item.SellIn < 0) return BadRequest("error: SellIn value must be greater than or equal to 0");
+            if (string.IsNullOrWhiteSpace(item.ItemName)) return BadRequest($"error: ItemName field must contain a value");
+
+            await _context.Items.AddAsync(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+
+        //Delete a single item
+        [HttpDelete("{DeleteAnItem}/{itemId}")]
+        public async Task<ActionResult> DeleteAnItem(int itemId)
+        {
+            var item = await _context.Items.AsNoTracking().FirstOrDefaultAsync(x => x.ItemId == itemId);
+            if (item == null) return NotFound();
+            _context.Items.Remove(item);
+            return Ok();
+        }
+
+        //Get All Categories
+        [HttpGet("{GetAllCategories}")]
+        public async Task<ActionResult<List<Category>>> GetAllCategories()
+        {
+            var categories = await _context.Categories.AsNoTracking().Where(x => true).ToListAsync();
+            if (categories == null) return NotFound();
+            return categories;
         }
 
     }
---
+++ GildedRose/Helpers/IncrementDayHandler.cs
@@ -0,0 +1,85 @@
+﻿using DataAccessLibrary;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GildedRose.Helpers
+{
+    public class IncrementDayHandler
+    {
+        private readonly InventoryContext _context;
+
+        public IncrementDayHandler(InventoryContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> IncrementOneDay()
+        {
+            var allItems = await _context.Items.Where(x=> true).ToListAsync();
+            var allCategories = await _context.Categories.Where(x=> true).ToListAsync();
+
+            var tempItemList = new List<Item>();
+
+            foreach(var item in allItems)
+            {
+                //always remove one day from SellIn
+                item.SellIn --;
+
+                //store items category for easy access
+                var category = allCategories.FirstOrDefault(x => x.CategoryId == item.CategoryId);
+
+                //If Legendary, take no action
+                if (category.IsLegendary)
+                {
+                    tempItemList.Add(item);
+                    continue;
+                }
+
+
+                if (item.QualityAppreciates && item.Quality < 50) //quality increases
+                {
+                    //Concert Ticket Quality
+                    if(category.CategoryName == "Backstage Passes")
+                    {
+                        switch (item.SellIn)
+                        {
+                            case int i when i > 10:
+                                item.Quality += 1;
+                                break;
+                            case int i when i <= 10 && i > 5:
+                                item.Quality += 2;
+                                break;
+                            case int i when i <= 5 && i > 0:
+                                item.Quality += 3;
+                                break;
+                            case int i when i <= 0:
+                                item.Quality = 0;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else //nested inside for future simple increments
+                    {
+                        if (item.ItemName == "Aged Brie")
+                            item.Quality++;
+                    }
+                }
+                else if (item.Quality != 0) //quality decreases
+                {
+                    //TODO hanle depreciation
+                }
+
+                tempItemList.Add(item);
+            }
+
+            //Update all items and save
+            _context.Items.UpdateRange(tempItemList);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
